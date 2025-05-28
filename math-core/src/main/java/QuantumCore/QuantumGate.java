227f5f4d21988324149f0ab6de30ce97c9eeac72
d4rk <herreragp334@gmail.com>
2025-05-28 10:01:11 +0100

    feat(QuantumCore) create QuantumGate class which implements QuantumGatePort

---
+++ math-core/src/main/java/QuantumCore/QuantumGate.java
@@ -0,0 +1,11 @@
+package QuantumCore;
+
+import QuantumCore.QuantumPorts.QuantumGatePort;
+
+public class QuantumGate implements QuantumGatePort {
+
+    @Override
+    public State apply(State state) {
+        return null;
+    }
+}
