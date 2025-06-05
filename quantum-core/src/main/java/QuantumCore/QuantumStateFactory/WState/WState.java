21573539058605be442ac9552d50654a68f818ef
d4rk <herreragp334@gmail.com>
2025-06-05 20:44:20 +0100

    feat (WState) develop the W state

---
+++ quantum-core/src/main/java/QuantumCore/QuantumStateFactory/WState/WState.java
@@ -0,0 +1,26 @@
+package QuantumCore.QuantumStateFactory.WState;
+
+import MathCore.Complex;
+import QuantumCore.Core.State;
+
+public class WState extends State {
+
+    public WState(int nQubits) {
+        super(createWStateAmplitudes(nQubits), nQubits);
+        if (nQubits < 2) throw new IllegalArgumentException("W State requires at least 2 qubits");
+    }
+
+    private static Complex[] createWStateAmplitudes(int nQubits) {
+        int size = (int) Math.pow(2, nQubits);
+        Complex[] amplitudes = new Complex[size];
+        double value = 1.0 / Math.sqrt(nQubits);
+        for (int i = 0; i < size; i++) {
+            amplitudes[i] = new Complex(0, 0);
+        }
+        for (int i = 0; i < nQubits; i++) {
+            int index = 1 << i;
+            amplitudes[index] = new Complex(value, 0);
+        }
+        return amplitudes;
+    }
+}
