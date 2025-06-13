3c875a092463a5c25549f1eb7c2eb21bfd3eddc2
d4rk <herreragp334@gmail.com>
2025-06-13 12:09:16 +0100

    delete (QuantumCore/QuantumPorts) delete getState useless method

--- quantum-core/src/main/java/QuantumCore/QuantumPorts/QubitPort.java
+++ quantum-core/src/main/java/QuantumCore/QuantumPorts/QubitPort.java
@@ -1,9 +1,7 @@
 package QuantumCore.QuantumPorts;
 import QuantumCore.Core.QuantumGate;
-import QuantumCore.Core.State;
 
 public interface QubitPort {
     int measure();
     void applyGate(QuantumGate gate);
-    State getState();
 }
