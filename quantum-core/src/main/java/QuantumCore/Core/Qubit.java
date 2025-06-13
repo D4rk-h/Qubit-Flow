9576409905fc796bbf0619e1f71e8625bcd71a16
d4rk <herreragp334@gmail.com>
2025-06-13 12:11:34 +0100

    fix (QuantumCore/Core) fix Qubit class implementation and functionality.

--- quantum-core/src/main/java/QuantumCore/Core/Qubit.java
+++ quantum-core/src/main/java/QuantumCore/Core/Qubit.java
@@ -1,7 +1,6 @@
 package QuantumCore.Core;
 import MathCore.Complex;
 import QuantumCore.QuantumPorts.QubitPort;
-
 import java.util.Random;
 
 public class Qubit implements QubitPort {
@@ -12,32 +11,32 @@ public class Qubit implements QubitPort {
     }
 
     public Qubit(Complex alpha, Complex beta) {
-        if (Math.abs(alpha.magnitude() + beta.magnitude() - 1.0)>1e-9){
+        if (Math.abs(alpha.magnitude() * alpha.magnitude() + beta.magnitude() * beta.magnitude() - 1.0)>1e-9){
             throw new IllegalArgumentException("Amplitudes must be normalized");
         }
         state = new State(alpha, beta);
     }
 
-    public State getState() {
-        return state;
-    }
+    public State getState() {return state;}
 
+    public void setState(State other){this.state=other;}
+
+    @Override
     public void applyGate(QuantumGate gate) {
         state = gate.apply(this.state);
     }
 
+    @Override
     public int measure() {
         Random random = new Random();
         double P0 = state.alpha().magnitude() * state.alpha().magnitude();
         double random_double = random.nextDouble(0.0, 1.0);
         if (random_double < P0) {
-            state = new State(new Complex(1.0, 0.0), new Complex(1.0, 0.0));
+            state = new State(new Complex(1.0, 0.0), new Complex(0.0, 0.0));
             return 0;
         } else {
             state = new State(new Complex(0.0, 0.0), new Complex(1.0, 0.0));
             return 1;
         }
     }
-
-
 }
