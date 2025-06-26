9d694163329f8c59dff23d43ca51c57c7ef9073e
d4rk <herreragp334@gmail.com>
2025-06-26 12:45:50 +0100

    feat(QuantumCore/QuantumGatesFactory/ControlledGate) create controlledGate class object as a 2-dimensional array composed by a control qubit and a quantum gate. TODO controlledGates have to extend controlledGate class

---
+++ quantum-core/src/main/java/QuantumCore/QuantumGatesFactory/ControlledGate/ControlledGate.java
@@ -0,0 +1,41 @@
+package QuantumCore.QuantumGatesFactory.ControlledGate;
+
+import QuantumCore.Core.QuantumGate;
+import QuantumCore.Core.Qubit;
+
+public class ControlledGate {
+    private Object[] controlledGate = new Object[2];
+    private Qubit controlQubit;
+    private QuantumGate gate;
+
+    public ControlledGate(Qubit controlQubit, QuantumGate gate){
+        Object[] conjugate = new Object[2];
+        conjugate[0] = controlQubit;
+        conjugate[1] = gate;
+        setControlledGate(conjugate);
+    }
+
+    public Object[] getControlledGate() {
+        return controlledGate;
+    }
+
+    public void setControlledGate(Object[] controlledGate) {
+        this.controlledGate = controlledGate;
+    }
+
+    public Qubit getControlQubit() {
+        return controlQubit;
+    }
+
+    public void setControlQubit(Qubit controlQubit) {
+        this.controlQubit = controlQubit;
+    }
+
+    public QuantumGate getGate() {
+        return gate;
+    }
+
+    public void setGate(QuantumGate gate) {
+        this.gate = gate;
+    }
+}
