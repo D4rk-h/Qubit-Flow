ad00e411cc3c2f6ff1f433b2bc793a9882a11612
d4rk <herreragp334@gmail.com>
2025-07-20 16:13:56 +0100

    fix: control/commands/simulationCommands/ create Simulate class that manages circuit simulation process

---
+++ quantum-core/src/main/java/control/commands/simulationCommands/Simulate.java
@@ -0,0 +1,25 @@
+package control.commands.simulationCommands;
+
+import control.commands.SimulationCommand;
+
+public class Simulate implements SimulationCommand {
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void abort() {
+
+    }
+}
