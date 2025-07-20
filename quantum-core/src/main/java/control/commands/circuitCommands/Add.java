6549de7b29f87a41818b958dbc347933bae8c64e
d4rk <herreragp334@gmail.com>
2025-07-20 16:13:19 +0100

    fix: control/commands/circuitCommands/ create Add, CreateCustom, Export, Import, Delete, DeleteCircuit classes which are all the commands that a circuit will have.

---
+++ quantum-core/src/main/java/control/commands/circuitCommands/Add.java
@@ -0,0 +1,30 @@
+package control.commands.circuitCommands;
+
+import control.commands.QuantumCommand;
+
+public class Add implements QuantumCommand {
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void undo() {
+
+    }
+
+    @Override
+    public boolean canUndo() {
+        return false;
+    }
+
+    @Override
+    public void redo() {
+
+    }
+
+    @Override
+    public String getDescription() {
+        return "";
+    }
+}
