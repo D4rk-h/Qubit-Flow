835af23a646d3c7874c8c623f2e6c82c84efd953
d4rk <herreragp334@gmail.com>
2025-07-29 16:56:46 +0100

    feat: circuitCommand/exportCommand/ develop execute() implementation of ExportJSON class using Jackson

--- quantum-core/src/main/java/control/command/circuitCommand/exportCommand/ExportJSON.java
+++ quantum-core/src/main/java/control/command/circuitCommand/exportCommand/ExportJSON.java
@@ -1,11 +1,24 @@
 package control.command.circuitCommand.exportCommand;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import control.command.QuantumCommand;
+import model.quantumModel.quantumCircuit.QuantumCircuit;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ExportJSON implements QuantumCommand {
+    private QuantumCircuit circuit;
+
     @Override
     public void execute() {
-
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String jsonCircuit = mapper.writeValueAsString(circuit.getCircuit());
+            mapper.writeValue(new File("./exported_circuit.json"), jsonCircuit);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
