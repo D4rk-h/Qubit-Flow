023d23d89082f8466ac6c0782486b43f273b2894
d4rk <herreragp334@gmail.com>
2025-07-29 14:36:09 +0100

    fix: measurementDisplay/chance transform VisualConfig class into BlochVisualizationConfig record

--- quantum-core/src/main/java/model/quantumModel/measurementDisplay/blochSphere/BlochVisualizationConfig.java
+++ quantum-core/src/main/java/model/quantumModel/measurementDisplay/blochSphere/BlochVisualizationConfig.java
@@ -14,28 +14,10 @@
 
 package model.quantumModel.measurementDisplay.blochSphere;
 
-public class VisualConfig {
-    private boolean isAnimating;
-    private double animationTime;
-
-    public VisualConfig(boolean isAnimating, double animationTime) {
-        this.animationTime = animationTime;
-        this.isAnimating = isAnimating;
-    }
-
-    public boolean isAnimating() {
-        return isAnimating;
-    }
-
-    public void setAnimating(boolean animating) {
-        isAnimating = animating;
-    }
-
-    public double getAnimationTime() {
-        return animationTime;
-    }
-
-    public void setAnimationTime(double animationTime) {
-        this.animationTime = animationTime;
-    }
-}
+public record BlochVisualizationConfig (
+        boolean isAnimated,
+        double animationDuration,
+        boolean showAxes,
+        boolean showGrid
+    )
+{}
