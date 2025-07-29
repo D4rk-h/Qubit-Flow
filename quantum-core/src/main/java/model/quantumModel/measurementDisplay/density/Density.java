37394b02dbb30be49d37cd3cb7df03ea1402147d
d4rk <herreragp334@gmail.com>
2025-07-29 14:35:16 +0100

    fix: measurementDisplay/chance develop Density class implementation

--- quantum-core/src/main/java/model/quantumModel/measurementDisplay/density/Density.java
+++ quantum-core/src/main/java/model/quantumModel/measurementDisplay/density/Density.java
@@ -1,4 +1,77 @@
+// Copyright 2025 D4rk-h
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package model.quantumModel.measurementDisplay.density;
 
-public class Density {
+import model.mathModel.Complex;
+import model.mathModel.Matrix;
+import model.quantumModel.QuantumState;
+import model.quantumModel.measurementDisplay.displayUtils.DisplayCategory;
+import model.quantumModel.measurementDisplay.displayUtils.DisplayPort;
+
+public class Density implements DisplayPort {
+    private final QuantumState targetState;
+    private final boolean showOnlyDiagonal;
+
+    public Density(QuantumState state, boolean showOnlyDiagonal) {
+        this.targetState = state;
+        this.showOnlyDiagonal = showOnlyDiagonal;
+    }
+
+    @Override
+    public String getDisplaySymbol() {
+        return DisplayCategory.DENSITY.getSymbol();
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Density Matrix Display";
+    }
+
+    @Override
+    public Object renderContent() {
+        Matrix densityMatrix = calculateDensityMatrix(targetState);
+        return showOnlyDiagonal ? extractDiagonal(densityMatrix) : densityMatrix;
+    }
+
+    @Override
+    public boolean isCompatibleWith(QuantumState state) {
+        return state.getNumQubits() > 6;
+    }
+
+    private Matrix calculateDensityMatrix(QuantumState state) {
+        Complex[] amplitudes = state.getAmplitudes();
+        int size = amplitudes.length;
+        Complex[][] density = new Complex[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                density[i][j] = amplitudes[i].multiply(amplitudes[j].conjugate());
+            }
+        }
+        return new Matrix(density);
+    }
+
+    private Matrix extractDiagonal(Matrix matrix) {
+        Matrix result = new Matrix(matrix.getRows(), matrix.getCols());
+        for (int i=0;i<matrix.getRows();i++) {
+            for (int j=0;j<matrix.getCols();j++) {
+                if (i==j){
+                    result.set(i, j, (Complex) matrix.get(i, j, true));
+                }
+                result.set(i, j, null);
+            }
+        }
+        return result;
+    }
 }
