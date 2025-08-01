26e525dd39dda73dc1ba6a122b2f22a5e078f87b
d4rk <herreragp334@gmail.com>
2025-08-01 17:17:43 +0100

    fix: circuitCommand/exportCommand fix json exporter class

--- quantum-core/src/main/java/control/command/circuitCommand/exportCommand/ExportJSON.java
+++ quantum-core/src/main/java/control/command/circuitCommand/exportCommand/ExportJSON.java
@@ -3,15 +3,16 @@ package control.command.circuitCommand.exportCommand;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import control.command.QuantumCommand;
 import model.quantumModel.quantumCircuit.QuantumCircuit;
+import model.quantumModel.quantumCircuit.quantumCircuitUtils.cliVisualization.QuantumCircuitCLIDisplay;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ExportJSON implements QuantumCommand {
     private final QuantumCircuit circuit;
+    public QuantumCircuitCLIDisplay utils = new QuantumCircuitCLIDisplay();
 
     public ExportJSON(QuantumCircuit circuit) {
         this.circuit = circuit;
@@ -21,17 +22,18 @@ public class ExportJSON implements QuantumCommand {
     public void execute() {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            Map<String, Object> data = new HashMap<>();
-            Map<String, List<Object>> circuitMap = new HashMap<>();
-            List<List<Object>> circuitData = circuit.getCircuit();
-            for (int i=0;i<circuitData.size();i++) {
-                String qubitKey = "qubit-" + (i + 1);
-                circuitMap.put(qubitKey, circuitData.get(i));
+            Map<String, Object> circuitData = new HashMap<>();
+            Map<String, String> wire = new HashMap<>();
+            for (int i=0;i<circuit.getCircuit().size();i++) {
+                String nWire = "wire-" + i;
+                for (int j=0;j<circuit.getCircuit().get(i).size();j++) {
+                    wire.put(nWire, utils.formatWire(circuit.getCircuit().get(i)));
+                }
+                circuitData.put("circuit", wire);
             }
-            data.put("circuit", circuitMap);
-            data.put("numOfQubits", circuit.getnQubits());
-            data.put("depth", circuit.getDepth());
-            mapper.writeValue(new File("./exported_circuit.json"), data);
+            circuitData.put("numOfQubits", circuit.getnQubits());
+            circuitData.put("depth", circuit.getDepth());
+            mapper.writeValue(new File("./exported_circuit.json"), circuitData);
         } catch (IOException e) {
             throw new RuntimeException("Error creating json response: "+ e.getMessage(), e);
         }
