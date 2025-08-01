ec12219bcdb4ab5c0dcb9b305edcc304088c8922
d4rk <herreragp334@gmail.com>
2025-08-01 11:59:25 +0100

    fix: developing ExportJSON class implementation.

--- quantum-core/src/main/java/control/command/circuitCommand/exportCommand/ExportJSON.java
+++ quantum-core/src/main/java/control/command/circuitCommand/exportCommand/ExportJSON.java
@@ -6,33 +6,52 @@ import model.quantumModel.quantumCircuit.QuantumCircuit;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ExportJSON implements QuantumCommand {
-    private QuantumCircuit circuit;
+    private final QuantumCircuit circuit;
+
+    public ExportJSON(QuantumCircuit circuit) {
+        this.circuit = circuit;
+    }
 
     @Override
     public void execute() {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            String jsonCircuit = mapper.writeValueAsString(circuit.getCircuit());
-            mapper.writeValue(new File("./exported_circuit.json"), jsonCircuit);
+            Map<String, Object> data = new HashMap<>();
+            Map<String, List<Object>> circuitMap = new HashMap<>();
+            List<List<Object>> circuitData = circuit.getCircuit();
+            for (int i=0;i<circuitData.size();i++) {
+                String qubitKey = "qubit-" + (i + 1);
+                circuitMap.put(qubitKey, circuitData.get(i));
+            }
+            data.put("circuit", circuitMap);
+            data.put("numOfQubits", circuit.getnQubits());
+            data.put("depth", circuit.getDepth());
+            mapper.writeValue(new File("./exported_circuit.json"), data);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error creating json response: "+ e.getMessage(), e);
         }
     }
 
     @Override
     public void undo() {
-
+        File exportedFile = new File("./exported_circuit.json");
+        if (exportedFile.exists()) {
+            exportedFile.delete();
+        }
     }
 
     @Override
     public boolean canUndo() {
-        return false;
+        return true;
     }
 
     @Override
     public void redo() {
-
+        execute();
     }
 }
