5f890fdc84c393d6271eaa0bc55844c1541245e4
d4rk <herreragp334@gmail.com>
2025-08-02 10:19:11 +0100

    del: quantumStates/ delete unnecessary complexity on quantum states, states needed to create others are just 0, 1, -, +, i, -i

--- .idea/fileTemplates/includes/File Header.java
+++ .idea/fileTemplates/includes/File Header.java
@@ -11,29 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package control.command.circuitCommand;
-
-import control.command.QuantumCommand;
-
-public class Import implements QuantumCommand {
-    @Override
-    public void execute() {
-
-    }
-
-    @Override
-    public void undo() {
-
-    }
-
-    @Override
-    public boolean canUndo() {
-        return false;
-    }
-
-    @Override
-    public void redo() {
-
-    }
-}
