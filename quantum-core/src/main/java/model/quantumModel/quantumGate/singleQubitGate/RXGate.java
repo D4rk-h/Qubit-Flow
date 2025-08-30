c317b9a4141ca636ba7617c93a547440b7ca29b6
d4rk-h <herreragp334@gmail.com>
2025-08-30 17:11:11 +0100

    feat: partially developed rotation gates, needs mayor improvement

--- quantum-core/src/main/java/model/quantumModel/quantumGate/singleQubitGate/RXGate.java
+++ quantum-core/src/main/java/model/quantumModel/quantumGate/singleQubitGate/RXGate.java
@@ -6,17 +6,18 @@ import model.quantumModel.quantumGate.QuantumGate;
 
 public class RXGate extends QuantumGate {
     private static double theta = Math.PI;
+
     public RXGate() {
-        super(buildRXGate(), 1, "RX (Rotation theta around x-axis)");
+        super(buildRXGate(), 1, "RX (Rotates theta around x-axis)");
     }
 
     private static Matrix buildRXGate() {
-        Complex[][] pauliX = new Complex[2][2];
-        pauliX[0][0] = new Complex (Math.cos(theta/2), 0);
-        pauliX[0][1] = new Complex (0, -Math.sin(theta/2));
-        pauliX[1][0] = new Complex (0, -Math.sin(theta/2));
-        pauliX[1][1] = new Complex (Math.cos(theta/2), 0);
-        return new Matrix(pauliX);
+        Complex[][] rx = new Complex[2][2];
+        rx[0][0] = new Complex (Math.cos(theta/2), 0);
+        rx[0][1] = new Complex (0, -Math.sin(theta/2));
+        rx[1][0] = new Complex (0, -Math.sin(theta/2));
+        rx[1][1] = new Complex (Math.cos(theta/2), 0);
+        return new Matrix(rx);
     }
 
     public double getTheta() {return theta;}
--- quantum-core/src/main/java/model/quantumModel/quantumGate/singleQubitGate/RZGate.java
+++ quantum-core/src/main/java/model/quantumModel/quantumGate/singleQubitGate/RZGate.java
@@ -1,4 +1,35 @@
 package model.quantumModel.quantumGate.singleQubitGate;
 
-public class RZGate {
+
+import model.mathModel.Complex;
+import model.mathModel.Matrix;
+import model.quantumModel.quantumGate.QuantumGate;
+
+public class RZGate extends QuantumGate {
+    private double phi = Math.PI;
+
+    public RZGate() {
+        super(buildRZGate(Math.PI), 1, "RZ (Rotates theta around Z-axis)");
+    }
+
+    public RZGate(double phi) {
+        super(buildRZGate(phi), 1, "RZ (Rotates theta around Z-axis)");
+        this.phi = phi;
+    }
+
+    private static Matrix buildRZGate(double phi) {
+        Complex[][] rz = new Complex[2][2];
+        rz[0][0] = Complex.exponential(-phi/2);
+        rz[0][1] = Complex.ZERO;
+        rz[1][0] = Complex.ZERO;
+        rz[1][1] = Complex.exponential(phi/2);
+        return new Matrix(rz);
+    }
+
+    public double getPhi() {return phi;}
+
+    public void setPhi(double phi) {
+        this.phi = phi;
+        new RZGate(phi);
+    }
 }
