51c2e4079faf21453788c3cc7156d42115e89e4b
d4rk-h <herreragp334@gmail.com>
2025-08-30 16:52:53 +0100

    feat: developed RXGate class

--- quantum-core/src/main/java/model/quantumModel/quantumGate/singleQubitGate/RXGate.java
+++ quantum-core/src/main/java/model/quantumModel/quantumGate/singleQubitGate/RXGate.java
@@ -1,4 +1,25 @@
 package model.quantumModel.quantumGate.singleQubitGate;
 
-public class RXGate {
+import model.mathModel.Complex;
+import model.mathModel.Matrix;
+import model.quantumModel.quantumGate.QuantumGate;
+
+public class RXGate extends QuantumGate {
+    private static double theta = Math.PI;
+    public RXGate() {
+        super(buildRXGate(), 1, "RX (Rotation theta around x-axis)");
+    }
+
+    private static Matrix buildRXGate() {
+        Complex[][] pauliX = new Complex[2][2];
+        pauliX[0][0] = new Complex (Math.cos(theta/2), 0);
+        pauliX[0][1] = new Complex (0, -Math.sin(theta/2));
+        pauliX[1][0] = new Complex (0, -Math.sin(theta/2));
+        pauliX[1][1] = new Complex (Math.cos(theta/2), 0);
+        return new Matrix(pauliX);
+    }
+
+    public double getTheta() {return theta;}
+
+    public void setTheta(double theta) {this.theta = theta;}
 }
